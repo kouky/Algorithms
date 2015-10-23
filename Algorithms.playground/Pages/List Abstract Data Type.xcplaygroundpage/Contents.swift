91937d2fbfeb070b2f0babe9eaa4627ae9d83794
Michael Koukoullis <mike@kouky.org>
2015-10-23 23:17:24 +1100

    Implement methods with foldl

--- Algorithms.playground/Pages/List Abstract Data Type.xcplaygroundpage/Contents.swift
+++ Algorithms.playground/Pages/List Abstract Data Type.xcplaygroundpage/Contents.swift
@@ -9,15 +9,6 @@ indirect enum List<T> {
     case Cons(T, List<T>)
     case Nil
     
-    func size() -> Int {
-        switch self {
-        case .Cons(_, let tail):
-            return 1 + tail.size()
-        case .Nil:
-            return 0
-        }
-    }
-    
     func head() -> T {
         switch self {
         case .Cons(let head, _):
@@ -36,22 +27,44 @@ indirect enum List<T> {
         }
     }
     
-    func isEmpty() -> Bool {
-        switch self {
-        case .Cons:
-            return false
-        case .Nil:
-            return true
-        }
-    }
-
-    func foldl<U>(acc: U, _ f: (U, T) -> U) -> U {
+    func foldl<U>(start: U, _ f: (U, T) -> U) -> U {
         switch self {
         case let .Cons(head, tail):
-            return tail.foldl(f(acc, head), f)
+            return tail.foldl(f(start, head), f)
         case .Nil:
-            return acc
+            return start
         }
+    }
+    
+    func size() -> Int {
+        return self.foldl(0) { (acc, _) -> Int in
+            return acc + 1
+        }
+    }
+    
+    func reverse() -> List {
+        return self.foldl(List.Nil) { (acc, item) -> List in
+            return List.Cons(item, acc)
+        }
+    }
+    
+    func filter(f: T -> Bool) -> List {
+        let x = self.foldl(List.Nil) { (acc, item) -> List in
+            if f(item) {
+                return List.Cons(item, acc)
+            }
+            else {
+                return acc
+            }
+        }
+        return x.reverse()
+    }
+    
+    func map<U>(f: T -> U) -> List<U> {
+        let x = self.foldl(List<U>.Nil) { (acc: List<U>, item: T) -> List<U> in
+            return List<U>.Cons(f(item), acc)
+        }
+        return x.reverse()
     }
     
     func print() -> String {
@@ -70,17 +83,17 @@ func ~ <T> (left: T, right: List<T>) -> List<T> {
     return List.Cons(left, right)
 }
 
-// Create lists of Int - these are equivalent
-var z = 10 ~ 20 ~ 30 ~ 40 ~ List.Nil
+// Create lists of Int
+let z = 1 ~ 2 ~ 3 ~ 4 ~ 5 ~ List.Nil
 z.print()
+
+// Create lists of tuples
+let x = ("grapes", 5) ~ ("apple", 8) ~ List.Nil
+x.print()
 
 // Head and Tail
 z.head()
 z.tail().print()
-
-// Size and Nil check
-z.size()
-z.isEmpty()
 
 // Foldl
 z.foldl(0, +)
@@ -89,3 +102,15 @@ z.foldl(List.Nil) { (acc, x) in
     return (x * 2) ~ acc
 }.print()
 
+// Functions implemeted with foldl
+z.size()
+z.reverse().print()
+z.filter { (item) -> Bool in
+    return item % 20 != 0
+}.print()
+z.map { (item) -> Int in
+    return item * 2
+}.print()
+z.map { (item) -> String in
+    return "$\(item).00"
+}.print()
