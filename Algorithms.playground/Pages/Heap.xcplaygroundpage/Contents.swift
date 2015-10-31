bdbb21e8dbcb39e85fd71b88f688f7f1ce1493cf
Michael Koukoullis <mike@kouky.org>
2015-10-31 23:54:32 +1100

    Heap data structure cleanups

--- Algorithms.playground/Pages/Heap.xcplaygroundpage/Contents.swift
+++ Algorithms.playground/Pages/Heap.xcplaygroundpage/Contents.swift
@@ -8,11 +8,10 @@
 
 import Darwin
 
-// Immutable
+// Full and almost full binary heaps may be represented (as an implicit data structure) using an array alone.
 struct Heap<T: Comparable> {
     private let items: [T]
     private let ordering: Ordering
-    
 }
 
 enum Ordering {
@@ -20,7 +19,6 @@ enum Ordering {
     case Max
 }
 
-// Public
 extension Heap {
     
     // Return highest priority element (i.e. root of heap)
@@ -36,12 +34,33 @@ extension Heap {
     
     // Delete the root from the heap, return the new heap
     func extract() -> Heap {
-        let newHeap = Heap(items: self.moveLastItemToFront(), ordering: ordering)
+        let newHeap = Heap(items: items.moveLastElementToFront(), ordering: ordering)
         return newHeap.downHeap(0)
     }
 }
 
-// Private
+// Implicit heap data strcuture can be navigated with index arithmentic
+extension Heap {
+    
+    private func parentIndexForIndex(index: Int) -> Int? {
+        guard index > 0 else {
+            return nil
+        }
+        return (index - 1) / 2
+    }
+    
+    private func rightChildIndexForIndex(index: Int) -> Int? {
+        let rightChildIndex = 2 * index + 2
+        return (rightChildIndex < items.endIndex) ? rightChildIndex : nil
+    }
+    
+    private func leftChildIndexForIndex(index: Int) -> Int? {
+        let leftChildIndex = 2 * index + 1
+        return (leftChildIndex < items.endIndex) ? leftChildIndex : nil
+    }
+}
+
+// Restore heap invariants
 extension Heap {
     
     private func upHeap(index: Int) -> Heap {
@@ -53,13 +72,13 @@ extension Heap {
         case .Min :
             if items[parentIndex] > items[index] {
                 print(self.description)
-                return Heap(items: self.swapItemsForIndices(parentIndex, index), ordering: ordering).upHeap(parentIndex)
+                return Heap(items: items.swapValuesForIndices(parentIndex, index), ordering: ordering).upHeap(parentIndex)
             } else {
                 return self
             }
         case .Max:
             if items[parentIndex] < items[index] {
-                return Heap(items: self.swapItemsForIndices(parentIndex, index), ordering: ordering).upHeap(parentIndex)
+                return Heap(items: items.swapValuesForIndices(parentIndex, index), ordering: ordering).upHeap(parentIndex)
             } else {
                 return self
             }
@@ -67,22 +86,21 @@ extension Heap {
     }
     
     private func downHeap(index: Int) -> Heap {
-        let (leftChildIndex, rightChildIndex) = self.childIndicesForIndex(index)
         
-        switch (leftChildIndex, rightChildIndex) {
+        switch (self.leftChildIndexForIndex(index), self.rightChildIndexForIndex(index)) {
         case (.None, .None):
             return self
         case (.Some(let left), .None):
             switch ordering {
             case .Min:
                 if items[left] < items[index] {
-                    return Heap(items: self.swapItemsForIndices(left, index), ordering: ordering).downHeap(left)
+                    return Heap(items: items.swapValuesForIndices(left, index), ordering: ordering).downHeap(left)
                 } else {
                     return self
                 }
             case .Max:
                 if items[left] > items[index] {
-                    return Heap(items: self.swapItemsForIndices(left, index), ordering: ordering).downHeap(left)
+                    return Heap(items: items.swapValuesForIndices(left, index), ordering: ordering).downHeap(left)
                 } else {
                     return self
                 }
@@ -91,13 +109,13 @@ extension Heap {
             switch ordering {
             case .Min:
                 if items[right] < items[index] {
-                    return Heap(items: self.swapItemsForIndices(right, index), ordering: ordering).downHeap(right)
+                    return Heap(items: items.swapValuesForIndices(right, index), ordering: ordering).downHeap(right)
                 } else {
                     return self
                 }
             case .Max:
                 if items[right] > items[index] {
-                    return Heap(items: self.swapItemsForIndices(right, index), ordering: ordering).downHeap(right)
+                    return Heap(items: items.swapValuesForIndices(right, index), ordering: ordering).downHeap(right)
                 } else {
                     return self
                 }
@@ -108,13 +126,13 @@ extension Heap {
             case .Min:
 
                 if items[left] < items[index] {
-                    return Heap(items: self.swapItemsForIndices(left, index), ordering: ordering).downHeap(left)
+                    return Heap(items: items.swapValuesForIndices(left, index), ordering: ordering).downHeap(left)
                 } else {
                     return self
                 }
             case .Max:
                 if items[right] > items[index] {
-                    return Heap(items: self.swapItemsForIndices(right, index), ordering: ordering).downHeap(right)
+                    return Heap(items: items.swapValuesForIndices(right, index), ordering: ordering).downHeap(right)
                 } else {
                     return self
                 }
@@ -123,68 +141,19 @@ extension Heap {
             switch ordering {
             case .Min:
                 if items[right] < items[index] {
-                    return Heap(items: self.swapItemsForIndices(right, index), ordering: ordering).downHeap(right)
+                    return Heap(items: items.swapValuesForIndices(right, index), ordering: ordering).downHeap(right)
                 } else {
                     return self
                 }
             case .Max:
                                 print("yo \(items[left]) \(items[right])")
                 if items[left] > items[index] {
-                    return Heap(items: self.swapItemsForIndices(left, index), ordering: ordering).downHeap(left)
+                    return Heap(items: items.swapValuesForIndices(left, index), ordering: ordering).downHeap(left)
                 } else {
                     return self
                 }
             }
         }
-    }
-
-    private func parentIndexForIndex(index: Int) -> Int? {
-        guard index > 0 else {
-            return Optional.None
-        }
-        return (index - 1) / 2
-    }
-    
-    private func childIndicesForIndex(index: Int) -> (Int?, Int?) {
-        return (self.leftChildIndexForIndex(index), self.rightChildIndexForIndex(index))
-    }
-    
-    private func rightChildIndexForIndex(index: Int) -> Int? {
-        let rightChildIndex = 2 * index + 2
-        if rightChildIndex < items.endIndex {
-            return rightChildIndex
-        } else {
-            return nil
-        }
-    }
-    
-    private func leftChildIndexForIndex(index: Int) -> Int? {
-        let leftChildIndex = 2 * index + 1
-        if leftChildIndex < items.endIndex {
-            return leftChildIndex
-        } else {
-            return nil
-        }
-    }
-
-    private func swapItemsForIndices(fromIndex: Int, _ toIndex: Int) -> [T] {
-        guard fromIndex >= 0 && toIndex >= 0 else {
-            fatalError("Invalid indices")
-        }
-        var newItems = items
-        newItems[fromIndex] = items[toIndex]
-        newItems[toIndex] = items[fromIndex]
-        return newItems
-    }
-    
-    private func moveLastItemToFront() -> [T] {
-        guard self.items.count > 1 else {
-            return self.items
-        }
-        
-        var newItems = Array(items[0..<(self.items.endIndex - 1)])
-        newItems[0] = self.items[self.items.endIndex - 1]
-        return newItems
     }
 }
 
@@ -194,7 +163,36 @@ extension Heap: CustomStringConvertible {
     }
 }
 
-let x = Heap<Int>(items: [], ordering: Ordering.Min)
+extension Array {
+    var lastIndex: Int {
+        return self.endIndex - 1
+    }
+    
+    var last: Element {
+        return self[self.lastIndex]
+    }
+    
+    private nonmutating func swapValuesForIndices(firstIndex: Int, _ secondIndex: Int) -> [Element] {
+        guard firstIndex >= 0 && secondIndex >= 0 else {
+            fatalError("Invalid indices")
+        }
+        var newItems = self
+        newItems[firstIndex] = self[secondIndex]
+        newItems[secondIndex] = self[firstIndex]
+        return newItems
+    }
+    
+    private nonmutating func moveLastElementToFront() -> [Element] {
+        guard self.count > 1 else {
+            return self
+        }
+        
+        var newElements = Array(self.dropLast())
+        newElements[0] = self.last
+        return newElements
+    }
+}
+
 // Min Heap
 let emptyMinHeap = Heap<Int>(items: [], ordering: Ordering.Min)
 let minHeap = emptyMinHeap.insert(10).insert(8).insert(2).insert(5).insert(7).insert(4)
